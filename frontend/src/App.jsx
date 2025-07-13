187687efab76aabd9d334384cea043fb7177f294
Grigor Stoyanov <grigors@localhost.localdomain>
2025-07-14 01:37:28 +0300

    refactors project architecture to include proper backend and containerised environment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,18 @@ import { useSyncQueue } from './Hooks/useSyncQueue';
 
 function App() {
   useSyncQueue()
+
+  const callExpress = () => {
+    fetch('http://localhost:5000').then(
+      (res)=>res.json()
+    ).then(data=>console.log(data))
+    .catch(error=>console.log(error));
+  }
   console.log('synced')
   return (
     <div className="p-4 w-screen">
      <SongSearchPage/>
+     <button onClick={callExpress}>CALL EXPRESSSSS!</button>
     </div>
   );
 }
